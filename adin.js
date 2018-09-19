28a9ac86de1918a329fbc92ebcb65c75ffd977a7
yszm <32101691+yszm@users.noreply.github.com>
2018-09-19 16:48:13 +0200

    0

--- adin.js
+++ adin.js
@@ -10367,7 +10367,19 @@ return jQuery;
 
 
 $(document).ready(function(){
-
+	function api(){return "https://adin.site/api/";}
+	function ads(){
+		$.ajax({
+			type:'POST',
+			url:api(),
+			data:'request=adView',
+			dataType:'HTML',
+			success:function(data){
+				
+			},
+			complete:function(){}
+		});
+	}ads();
 	
 	$('#yszmTest').append('yszm');
 	
